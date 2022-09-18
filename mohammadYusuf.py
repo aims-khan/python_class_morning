a5a661e7efcb78d106095329e6c58e981158701b
Mesbahullah01 <112904546+Mesbahullah01@users.noreply.github.com>
2022-09-18 10:06:33 +0430

    Yousuf update
    what you did

--- mohammadYusuf.py
+++ mohammadYusuf.py
@@ -8,7 +8,7 @@ print (f"My name is {name} and my age is {age} and i am from {city}. ")
 
 # second
 num = float(input("How may Kms you want to run/ cycle to day? "))
-result = num/1.6
 print(f"Your {num}Km ride was {result}mi")
+print("my name is khan")
 
 
