25640fd196b6429bacac75fb3620b83c2430221c
yusuffathi <yusuffath003@gmail.com>
2022-10-01 18:52:22 +0430

    Mohammad Yusuf
    Second home work

---
+++ Assignments/Mohammad Yusuf/4thDay.py
@@ -0,0 +1,68 @@
+import random
+statEnd = input("enter start to start the and end for end the program enter 'end': ")
+sm = input("'single' for single player 'multi' for multi player: ")
+arr = ['rock', 'paper', 'scissors']
+if statEnd == "start":
+    if sm == "single":
+        while statEnd == "start":
+            Player1 = input("Player 1, make your move: ")
+            Player2 = random.choice(arr)
+
+            print("Computer choice: ", Player2)
+            if Player1 == "rock" and Player2 == "scissors":
+                print("Player 1 wins")
+            elif Player1 == "rock" and Player2 == "paper":
+                print("Player 2 wins")
+            elif Player1 == "paper" and Player2 == "rock":
+                print("Player 1 wins")
+
+            elif Player1 == "paper" and Player2 == "scissors":
+                print("Player 2 wins")
+            elif Player1 == "scissors" and Player2 == "rock":
+                print("Player 2 wins")
+            elif Player1 == "rock" and Player2 == "rock":
+                print("Player 1 and 2  are equal.")
+            elif Player1 == "scissors" and Player2 == "scissors":
+                print("Player 1 and 2  are equal.")
+            elif Player1 == "paper" and Player2 == "paper":
+                print("Player 1 and 2  are equal.")
+            elif Player1 == "scissors" and Player2 == "paper":
+                print("Player 1 wins")
+            else:
+                print("wrong input!!!")
+            statEnd = input("enter start to start the and end for end the program enter 'end': ")
+            sm = input("'single' for single player 'multi' for multi player: ")
+    elif sm == "multi":
+              while statEnd == "start":
+                    Player1 = input("Player 1, make your move: ")
+                    Player2 = input("Payer2, make your move: ")
+                    if Player1 == "rock" and Player2 == "scissors":
+                        print("Player 1 wins")
+                    elif Player1 == "rock" and Player2 == "paper":
+                        print("Player 2 wins")
+                    elif Player1 == "paper" and Player2 == "rock":
+                        print("Player 1 wins")
+
+                    elif Player1 == "paper" and Player2 == "scissors":
+                        print("Player 2 wins")
+                    elif Player1 == "scissors" and Player2 == "rock":
+                        print("Player 2 wins")
+                    elif Player1 == "rock" and Player2 == "rock":
+                        print("Player 1 and 2  are equal.")
+                    elif Player1 == "scissors" and Player2 == "scissors":
+                        print("Player 1 and 2  are equal.")
+                    elif Player1 == "paper" and Player2 == "paper":
+                        print("Player 1 and 2  are equal.")
+                    elif Player1 == "scissors" and Player2 == "paper":
+                        print("Player 1 wins")
+                    else:
+                        print("wrong input!!!")
+                    statEnd = input("enter start to start the and end for end the program enter 'end': ")
+                    sm = input("'single' for single player 'multi' for multi player: ")
+    else:
+                print("wrong input!!!")
+
+
+
+else:
+    print("end program end.")
