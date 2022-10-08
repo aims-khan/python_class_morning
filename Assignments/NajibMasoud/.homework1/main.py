b3834d6cb2e4e90d1c5dc4616b5c06e900e01f18
Najibmasoud <97896138+amiri002@users.noreply.github.com>
2022-10-08 22:25:28 +0430

    home work1

---
+++ Assignments/NajibMasoud/.homework1/main.py
@@ -0,0 +1,15 @@
+color = input("Enter your favorite color: ")
+if color == "gray":
+    print("Not Bad!")
+
+elif color == "red":
+    print("Good one.")
+
+elif color == "black":
+    print("superb choice.")
+
+elif color == "pink":
+    print("you like it no problem.")
+
+else :
+    print("Your choice is not bad.")
