306a018e303c4a76ec1d5c29e03aa87812b5dd19
Buniadi <113782370+Buniadi@users.noreply.github.com>
2022-10-08 07:13:22 -0700

    My both Assignment
    The first is find the bigger one in an array and second one is to make recatangle with emojie

--- Assignments/Obaidullah/main.py
+++ Assignments/Obaidullah/main.py
@@ -4,6 +4,17 @@ while num<10:
      num+=1
 
 
+# Second Homework
+
+line=4
+for i in range(0,line):
+    for j in range(0,line-i-1):
+        print(end=" ")
+    for j in range(0,i+1):
+        print("\U0001f600",end=" ")
+    print()
+
+
 
 
 
